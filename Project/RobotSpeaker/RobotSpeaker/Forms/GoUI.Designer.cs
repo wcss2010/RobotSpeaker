d42d45488a1788c5f0e3ef4be77f6d055ebc0d83
wcss <wcss2020@gmail.com>
2018-07-03 00:53:00 +0800

    .....

--- Project/RobotSpeaker/RobotSpeaker/Forms/GoUI.Designer.cs
+++ Project/RobotSpeaker/RobotSpeaker/Forms/GoUI.Designer.cs
@@ -28,21 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.plTabPanel = new System.Windows.Forms.Panel();
+            this.ibFree = new RobotSpeaker.Controls.ImageButton();
+            this.ibUseDevice = new RobotSpeaker.Controls.ImageButton();
+            this.ibNormal = new RobotSpeaker.Controls.ImageButton();
+            this.plTabPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // plTabPanel
+            // 
+            this.plTabPanel.Controls.Add(this.ibFree);
+            this.plTabPanel.Controls.Add(this.ibUseDevice);
+            this.plTabPanel.Controls.Add(this.ibNormal);
+            this.plTabPanel.Location = new System.Drawing.Point(43, 143);
+            this.plTabPanel.Name = "plTabPanel";
+            this.plTabPanel.Size = new System.Drawing.Size(572, 200);
+            this.plTabPanel.TabIndex = 11;
+            // 
+            // ibFree
+            // 
+            this.ibFree.BackColor = System.Drawing.Color.Transparent;
+            this.ibFree.BottomText = "自由导航模式";
+            this.ibFree.BottomTextColor = System.Drawing.Color.White;
+            this.ibFree.BottomTextFont = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ibFree.BottomTextHeight = 35;
+            this.ibFree.EnabledTextLabel = true;
+            this.ibFree.FocusImage = null;
+            this.ibFree.Location = new System.Drawing.Point(392, 0);
+            this.ibFree.Name = "ibFree";
+            this.ibFree.NoFocusImage = null;
+            this.ibFree.Size = new System.Drawing.Size(178, 200);
+            this.ibFree.TabIndex = 9;
+            this.ibFree.Click += new System.EventHandler(this.ibFree_Click);
+            // 
+            // ibUseDevice
+            // 
+            this.ibUseDevice.BackColor = System.Drawing.Color.Transparent;
+            this.ibUseDevice.BottomText = "遥控模式";
+            this.ibUseDevice.BottomTextColor = System.Drawing.Color.White;
+            this.ibUseDevice.BottomTextFont = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ibUseDevice.BottomTextHeight = 35;
+            this.ibUseDevice.EnabledTextLabel = true;
+            this.ibUseDevice.FocusImage = null;
+            this.ibUseDevice.Location = new System.Drawing.Point(197, 0);
+            this.ibUseDevice.Name = "ibUseDevice";
+            this.ibUseDevice.NoFocusImage = null;
+            this.ibUseDevice.Size = new System.Drawing.Size(178, 200);
+            this.ibUseDevice.TabIndex = 8;
+            this.ibUseDevice.Click += new System.EventHandler(this.ibUseDevice_Click);
+            // 
+            // ibNormal
+            // 
+            this.ibNormal.BackColor = System.Drawing.Color.Transparent;
+            this.ibNormal.BottomText = "静止模式(默认)";
+            this.ibNormal.BottomTextColor = System.Drawing.Color.White;
+            this.ibNormal.BottomTextFont = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ibNormal.BottomTextHeight = 35;
+            this.ibNormal.EnabledTextLabel = true;
+            this.ibNormal.FocusImage = null;
+            this.ibNormal.Location = new System.Drawing.Point(0, 0);
+            this.ibNormal.Name = "ibNormal";
+            this.ibNormal.NoFocusImage = null;
+            this.ibNormal.Size = new System.Drawing.Size(178, 200);
+            this.ibNormal.TabIndex = 7;
+            this.ibNormal.Click += new System.EventHandler(this.ibNormal_Click);
             // 
             // GoUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1031, 487);
+            this.Controls.Add(this.plTabPanel);
             this.Name = "GoUI";
             this.Text = "GoUI";
             this.TitleText = "运动模式";
             this.TitleTextColor = System.Drawing.Color.White;
+            this.Controls.SetChildIndex(this.plTabPanel, 0);
+            this.plTabPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel plTabPanel;
+        private Controls.ImageButton ibFree;
+        private Controls.ImageButton ibUseDevice;
+        private Controls.ImageButton ibNormal;
     }
 }
--- Project/RobotSpeaker/RobotSpeaker/Forms/GoUI.cs
+++ Project/RobotSpeaker/RobotSpeaker/Forms/GoUI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -19,6 +20,18 @@ namespace RobotSpeaker.Forms
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
+
+            plTabPanel.Top = (Screen.PrimaryScreen.Bounds.Height - plTabPanel.Height) / 2;
+            plTabPanel.Left = (Screen.PrimaryScreen.Bounds.Width - plTabPanel.Width) / 2;
+
+            ibNormal.NoFocusImage = Image.FromFile(Path.Combine(Application.StartupPath, @"Images\face1.png"));
+            ibNormal.FocusImage = Image.FromFile(Path.Combine(Application.StartupPath, @"Images\face2.png"));
+
+            ibUseDevice.NoFocusImage = Image.FromFile(Path.Combine(Application.StartupPath, @"Images\voice1.png"));
+            ibUseDevice.FocusImage = Image.FromFile(Path.Combine(Application.StartupPath, @"Images\voice2.png"));
+
+            ibFree.NoFocusImage = Image.FromFile(Path.Combine(Application.StartupPath, @"Images\set1.png"));
+            ibFree.FocusImage = Image.FromFile(Path.Combine(Application.StartupPath, @"Images\set2.png"));
         }
 
         protected override void OnClickBackButton(EventArgs e)
@@ -26,6 +39,21 @@ namespace RobotSpeaker.Forms
             base.OnClickBackButton(e);
 
             Close();
+        }
+
+        private void ibNormal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ibUseDevice_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ibFree_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
