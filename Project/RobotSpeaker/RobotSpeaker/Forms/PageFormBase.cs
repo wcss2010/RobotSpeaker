db9057b9cf7a5bb72692ea6bd44aa4f6f0b7c215
wcss <wcss2020@gmail.com>
2018-07-02 22:32:06 +0800

    .............

---
+++ Project/RobotSpeaker/RobotSpeaker/Forms/PageFormBase.cs
@@ -0,0 +1,30 @@
+﻿using RobotSpeaker.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RobotSpeaker.Forms
+{
+    public partial class PageFormBase : ContentFormBase
+    {
+        public PageFormBase()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            TitleLabel.ForeColor = Color.White;
+            BackButton.NoFocusImage = Image.FromFile(Path.Combine(Application.StartupPath, @"Images\goback.png"));
+            LogoBox.Image = Image.FromFile(Path.Combine(Application.StartupPath, @"Images\logo.png"));
+        }
+    }
+}
