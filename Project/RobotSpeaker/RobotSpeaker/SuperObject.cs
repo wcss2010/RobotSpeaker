e829d47964182cd9a384f5cf0d578f9c615cffbf
wcss2010 <wcss2020@gmail.com>
2018-07-03 13:42:38 +0800

    。。。。

---
+++ Project/RobotSpeaker/RobotSpeaker/SuperObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RobotSpeaker
+{
+    public class SuperObject
+    {
+        /// <summary>
+        /// 自我介绍
+        /// </summary>
+        public static string ReadmeDir = Path.Combine(Application.StartupPath, "readme");
+
+        /// <summary>
+        /// 摄像头
+        /// </summary>
+        public static string CameraDir = Path.Combine(Application.StartupPath, "camera");
+
+        /// <summary>
+        /// 照片
+        /// </summary>
+        public static string CameraPhotoDir = Path.Combine(CameraDir, "photo");
+
+        /// <summary>
+        /// 视频
+        /// </summary>
+        public static string CameraVideoDir = Path.Combine(CameraDir, "video");
+    }
+}
