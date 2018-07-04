7e1ca08bc5318478a3e1d56908fe0a107b2631b6
wcss2010 <wcss2020@gmail.com>
2018-07-04 10:07:01 +0800

    。。。

--- Project/RobotSpeaker/RobotSpeaker/Forms/FaceUI.Designer.cs
+++ Project/RobotSpeaker/RobotSpeaker/Forms/FaceUI.Designer.cs
@@ -28,21 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.btnStopRecord = new RobotSpeaker.Controls.ButtonExt();
+            this.btnPauseRecord = new RobotSpeaker.Controls.ButtonExt();
+            this.btnGetPic = new RobotSpeaker.Controls.ImageButton();
+            this.btnRecord = new RobotSpeaker.Controls.ImageButton();
+            this.plMiddleButtons = new System.Windows.Forms.Panel();
+            this.plContent = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.plMiddleButtons.SuspendLayout();
+            this.plContent.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pbImage
+            // 
+            this.pbImage.Location = new System.Drawing.Point(24, 19);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(320, 640);
+            this.pbImage.TabIndex = 1;
+            this.pbImage.TabStop = false;
+            // 
+            // btnStopRecord
+            // 
+            this.btnStopRecord.ButtonNormalBackColor = System.Drawing.Color.Transparent;
+            this.btnStopRecord.ButtonNormalForeColor = System.Drawing.Color.White;
+            this.btnStopRecord.ButtonPressedBackColor = System.Drawing.Color.Orange;
+            this.btnStopRecord.ButtonPressedForeColor = System.Drawing.Color.White;
+            this.btnStopRecord.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnStopRecord.Location = new System.Drawing.Point(28, 609);
+            this.btnStopRecord.Name = "btnStopRecord";
+            this.btnStopRecord.Size = new System.Drawing.Size(80, 50);
+            this.btnStopRecord.TabIndex = 2;
+            this.btnStopRecord.Text = "停止";
+            this.btnStopRecord.Visible = false;
+            this.btnStopRecord.Click += new System.EventHandler(this.btnStopRecord_Click);
+            // 
+            // btnPauseRecord
+            // 
+            this.btnPauseRecord.ButtonNormalBackColor = System.Drawing.Color.Transparent;
+            this.btnPauseRecord.ButtonNormalForeColor = System.Drawing.Color.White;
+            this.btnPauseRecord.ButtonPressedBackColor = System.Drawing.Color.Orange;
+            this.btnPauseRecord.ButtonPressedForeColor = System.Drawing.Color.White;
+            this.btnPauseRecord.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnPauseRecord.Location = new System.Drawing.Point(266, 609);
+            this.btnPauseRecord.Name = "btnPauseRecord";
+            this.btnPauseRecord.Size = new System.Drawing.Size(80, 50);
+            this.btnPauseRecord.TabIndex = 3;
+            this.btnPauseRecord.Text = "暂停";
+            this.btnPauseRecord.Visible = false;
+            this.btnPauseRecord.Click += new System.EventHandler(this.btnPauseRecord_Click);
+            // 
+            // btnGetPic
+            // 
+            this.btnGetPic.BottomText = "拍    照";
+            this.btnGetPic.BottomTextColor = System.Drawing.Color.White;
+            this.btnGetPic.BottomTextFont = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnGetPic.BottomTextHeight = 35;
+            this.btnGetPic.EnabledTextLabel = true;
+            this.btnGetPic.FocusImage = null;
+            this.btnGetPic.Location = new System.Drawing.Point(3, 3);
+            this.btnGetPic.Name = "btnGetPic";
+            this.btnGetPic.NoFocusImage = null;
+            this.btnGetPic.Size = new System.Drawing.Size(177, 164);
+            this.btnGetPic.TabIndex = 4;
+            this.btnGetPic.Click += new System.EventHandler(this.btnGetPic_Click);
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.BottomText = "开始录像";
+            this.btnRecord.BottomTextColor = System.Drawing.Color.White;
+            this.btnRecord.BottomTextFont = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnRecord.BottomTextHeight = 35;
+            this.btnRecord.EnabledTextLabel = true;
+            this.btnRecord.FocusImage = null;
+            this.btnRecord.Location = new System.Drawing.Point(3, 224);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.NoFocusImage = null;
+            this.btnRecord.Size = new System.Drawing.Size(177, 164);
+            this.btnRecord.TabIndex = 4;
+            this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
+            // 
+            // plMiddleButtons
+            // 
+            this.plMiddleButtons.Controls.Add(this.btnGetPic);
+            this.plMiddleButtons.Controls.Add(this.btnRecord);
+            this.plMiddleButtons.Location = new System.Drawing.Point(457, 72);
+            this.plMiddleButtons.Name = "plMiddleButtons";
+            this.plMiddleButtons.Size = new System.Drawing.Size(183, 385);
+            this.plMiddleButtons.TabIndex = 5;
+            // 
+            // plContent
+            // 
+            this.plContent.Controls.Add(this.btnPauseRecord);
+            this.plContent.Controls.Add(this.btnStopRecord);
+            this.plContent.Controls.Add(this.plMiddleButtons);
+            this.plContent.Controls.Add(this.pbImage);
+            this.plContent.Location = new System.Drawing.Point(181, 76);
+            this.plContent.Name = "plContent";
+            this.plContent.Size = new System.Drawing.Size(661, 680);
+            this.plContent.TabIndex = 6;
             // 
             // FaceUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1031, 487);
+            this.ClientSize = new System.Drawing.Size(1031, 768);
+            this.Controls.Add(this.plContent);
             this.Name = "FaceUI";
             this.Text = "FaceUI";
             this.TitleText = "摄像模式";
             this.TitleTextColor = System.Drawing.Color.White;
+            this.Controls.SetChildIndex(this.plContent, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.plMiddleButtons.ResumeLayout(false);
+            this.plContent.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbImage;
+        private Controls.ButtonExt btnStopRecord;
+        private Controls.ButtonExt btnPauseRecord;
+        private Controls.ImageButton btnGetPic;
+        private Controls.ImageButton btnRecord;
+        private System.Windows.Forms.Panel plMiddleButtons;
+        private System.Windows.Forms.Panel plContent;
     }
 }
--- Project/RobotSpeaker/RobotSpeaker/Forms/FaceUI.cs
+++ Project/RobotSpeaker/RobotSpeaker/Forms/FaceUI.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -22,6 +23,20 @@ namespace RobotSpeaker.Forms
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
+
+            //设置居中
+            plContent.Top = (Screen.PrimaryScreen.Bounds.Height - plContent.Height) / 2;
+            plContent.Left = (Screen.PrimaryScreen.Bounds.Width - plContent.Width) / 2;
+
+            btnGetPic.NoFocusImage = Image.FromFile(Path.Combine(Application.StartupPath, @"Images\pic1.png"));
+            btnGetPic.FocusImage = Image.FromFile(Path.Combine(Application.StartupPath, @"Images\pic2.png"));
+
+            btnRecord.NoFocusImage = Image.FromFile(Path.Combine(Application.StartupPath, @"Images\video1.png"));
+            btnRecord.FocusImage = Image.FromFile(Path.Combine(Application.StartupPath, @"Images\video2.png"));
+
+            //启动摄像头
+            videoObj = new Video(pbImage.Handle, pbImage.Width, pbImage.Height);
+            videoObj.StartWebCam();
         }
 
         protected override void OnClickBackButton(EventArgs e)
@@ -29,6 +44,50 @@ namespace RobotSpeaker.Forms
             base.OnClickBackButton(e);
 
             Close();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            //停止录像
+            try
+            {
+                videoObj.StopKinescope();
+            }
+            catch (Exception ex) { }
+
+            //关闭摄像头
+            try
+            {
+                videoObj.CloseWebcam(0);
+            }
+            catch (Exception ex) { }
+        }
+
+        private void btnPauseRecord_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStopRecord_Click(object sender, EventArgs e)
+        {
+            videoObj.StopKinescope();
+            btnStopRecord.Visible = false;
+            btnRecord.Enabled = true;
+        }
+
+        private void btnGetPic_Click(object sender, EventArgs e)
+        {
+            videoObj.grabImage(Path.Combine(SuperObject.CameraPhotoDir, DateTime.Now.Ticks + ".bmp"));
+            MessageBox.Show("操作完成！");
+        }
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {            
+            videoObj.StarKinescope(Path.Combine(SuperObject.CameraPhotoDir, DateTime.Now.Ticks + ".avi"));
+            btnStopRecord.Visible = true;
+            btnRecord.Enabled = false;
         }
     }
 }
