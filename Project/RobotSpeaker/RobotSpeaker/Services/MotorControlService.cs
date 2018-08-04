06fa2d7737626520357b7f97a4d0c461b340d99f
wcss2020@gmail.com <wcss2020@gmail.com>
2018-08-04 16:09:55 +0800

    ,.

--- Project/RobotSpeaker/RobotSpeaker/Services/MotorControlService.cs
+++ Project/RobotSpeaker/RobotSpeaker/Services/MotorControlService.cs
@@ -34,7 +34,7 @@ namespace RobotSpeaker
 
         public void Open()
         {
-            _motorPort.SetPort(SuperObject.Config.GoPort, 115200, System.IO.Ports.StopBits.One, System.IO.Ports.Parity.None, -1, -1);
+            _motorPort.SetPort(SuperObject.Config.GoPort, 115200, System.IO.Ports.StopBits.One, System.IO.Ports.Parity.None, 150, 150);
             _motorPort.MessageReceived += _motorPost_MessageReceived;
             _motorPort.Connect();
         }
