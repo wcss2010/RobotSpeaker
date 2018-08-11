026f1de916b4d31956289da957b31e4b63d2e8a0
wcss <wcss2010@qq.com>
2018-08-11 13:38:11 +0800

    ......

--- Project/RobotSpeaker/RobotSpeaker/Services/MessageDataAdapters/MotorControlDataAdapter.cs
+++ Project/RobotSpeaker/RobotSpeaker/Services/MessageDataAdapters/MotorControlDataAdapter.cs
@@ -10,7 +10,27 @@ namespace RobotSpeaker
     {
         public override byte[] Resolve()
         {
-            
+            byte[] results = new byte[0];
+            List<byte> dataList = SerialPortInputObject.BufferStream;
+            if (dataList.Count >= 8)
+            {
+                if (dataList[0] == 0x01 && dataList[1] == 0x06)
+                {
+                    results = dataList.GetRange(0, 8).ToArray();
+                }
+                else
+                {
+                    dataList.RemoveAt(0);
+                }
+            }
+
+            //清空数据
+            lock (SerialPortInput.lockObject)
+            {
+                SerialPortInputObject.BufferStream.Clear();
+            }
+
+            return results;
         }
     }
 }
