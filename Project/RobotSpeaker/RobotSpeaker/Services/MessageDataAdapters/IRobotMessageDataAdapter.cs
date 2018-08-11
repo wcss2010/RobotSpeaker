54d53c8f9d1ee10076d21fe814e4e93dcfe22913
wcss <wcss2010@qq.com>
2018-08-11 18:19:02 +0800

    ...

---
+++ Project/RobotSpeaker/RobotSpeaker/Services/MessageDataAdapters/IRobotMessageDataAdapter.cs
@@ -0,0 +1,16 @@
+﻿using SerialPortLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RobotSpeaker.Services.MessageDataAdapters
+{
+    /// <summary>
+    /// 消息适配器基类
+    /// </summary>
+    public abstract class IRobotMessageDataAdapter:IMessageDataAdapter
+    {
+        
+    }
+}
