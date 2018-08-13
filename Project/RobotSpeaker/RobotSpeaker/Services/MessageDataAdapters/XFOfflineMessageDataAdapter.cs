58f1fc42b93c7ad7bf246b46d28b0b199fb08526
wcss <wcss2010@qq.com>
2018-08-13 19:55:55 +0800

    .......

--- Project/RobotSpeaker/RobotSpeaker/Services/MessageDataAdapters/XFOfflineMessageDataAdapter.cs
+++ Project/RobotSpeaker/RobotSpeaker/Services/MessageDataAdapters/XFOfflineMessageDataAdapter.cs
@@ -9,66 +9,49 @@ namespace RobotSpeaker
 {
     public class XFOfflineMessageDataAdapter : IRobotMessageDataAdapter
     {
-        bool resAssembling = false;
-        int headerIndex = 0;
+        private byte[] _headerBytes = new byte[] { 0xFD, 0x00, 0x80, 0x00 };
 
         public override byte[] Resolve()
         {
             try
             {
                 List<byte> _recievedData = this.SerialPortInputObject.BufferStream;
-                if (!resAssembling)
+                int headerIndex = SearchInBuffer(_headerBytes);
+                if (headerIndex >= 0)
                 {
-                    while (headerIndex + 3 < _recievedData.Count && !(_recievedData[headerIndex] == 0xFD && _recievedData[headerIndex + 1] == 0x00 && _recievedData[headerIndex + 2] == 0x80 && _recievedData[headerIndex + 3] == 0x00))
+                    //有效
+                    // 帧长度=数据区长度+1
+                    int length = 264;
+                    if (headerIndex + length > _recievedData.Count)
                     {
-                        headerIndex++;
+                        return new byte[0];
                     }
+
+                    //取数据
+                    byte[] bytes = _recievedData.GetRange(headerIndex, length).ToArray();
+
+                    //删除解析过的部分
                     lock (SerialPortInput.lockObject)
                     {
-                        if (headerIndex >= _recievedData.Count)
-                        {
-                            _recievedData.Clear();
-                        }
+                        _recievedData.RemoveRange(0, headerIndex + length);
                     }
-                    resAssembling = true;
-                }
 
-                if (headerIndex + 2 >= _recievedData.Count)
-                {
-                    Thread.Sleep(10);
-                }
-
-                // 帧长度=数据区长度+1
-                int length = 264;
-                if (headerIndex + length > _recievedData.Count)
-                {
-                    Thread.Sleep(10);
-                }
-
-                //取数据
-                byte[] bytes = _recievedData.GetRange(headerIndex, 264).ToArray();
-
-                //删除解析过的部分
-                lock (SerialPortInput.lockObject)
-                {
-                    _recievedData.RemoveRange(0, headerIndex + length);
-                }
-
-                resAssembling = false;
-                headerIndex = 0;
-
-                if (bytes.Length >= 264)
-                {
                     return bytes;
+
                 }
                 else
                 {
+                    //无效数据
+                    lock (SerialPortInput.lockObject)
+                    {
+                        _recievedData.Clear();
+                    }
+
                     return new byte[0];
                 }
             }
             catch (Exception ex)
             {
-                headerIndex = 0;
                 System.Console.WriteLine(ex.ToString());
 
                 return new byte[0];
