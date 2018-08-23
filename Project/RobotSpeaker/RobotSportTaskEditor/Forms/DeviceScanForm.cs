c7e3ab3169e96a3ff1fdb442c24e3f5cec1071fe
wcss <wcss2010@qq.com>
2018-08-23 16:34:28 +0800

    ...

--- Project/RobotSpeaker/RobotSportTaskEditor/Forms/DeviceScanForm.cs
+++ Project/RobotSpeaker/RobotSportTaskEditor/Forms/DeviceScanForm.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using System.Linq;
 using System.Management;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -21,21 +22,146 @@ namespace RobotSportTaskEditor.Forms
         /// </summary>
         public List<RobotConfigItem> SelectedList { get; set; }
 
+        private List<RobotConfigItem> totalRobotList = new List<RobotConfigItem>(); 
+
         public DeviceScanForm(DeviceListForm deviceListForm)
         {
             InitializeComponent();
 
-            DeviceForm = deviceListForm;
+            DeviceListF = deviceListForm;
+
+            #region 广播地址
             string lip = _udpPortScan.UdpClient.GetLocalIP();
             string[] ttt = lip.Split('.');
             tbUdpBoardCastIp.Text = ttt[0] + "." + ttt[1] + "." + ttt[2] + ".255";
+            #endregion
+
+            _udpPortScan.RobotResponseEvent += _udpPortScan_RobotResponseEvent;
+            _udpPortScan.StartEvent += _udpPortScan_StartEvent;
+            _udpPortScan.StopEvent += _udpPortScan_StopEvent;
+            _udpPortScan.ProgressEvent += _udpPortScan_ProgressEvent;
         }
 
-        public DeviceListForm DeviceForm { get; set; }
+        void _udpPortScan_ProgressEvent(object sender, RobotFinderLibrary.ProgressEventArgs args)
+        {
+
+        }
+
+        void _udpPortScan_StopEvent(object sender, EventArgs args)
+        {
+            try
+            {
+                Thread.Sleep(8000);
+            }
+            catch (Exception ex) { }
+
+            if (IsHandleCreated)
+            {
+                Invoke(new MethodInvoker(delegate()
+                {
+                    btnStartScan.Enabled = true;
+                }));
+            }
+        }
+
+        void _udpPortScan_StartEvent(object sender, EventArgs args)
+        {
+            if (IsHandleCreated)
+            {
+                Invoke(new MethodInvoker(delegate()
+                    {
+                        btnStartScan.Enabled = false;
+                    }));
+            }
+        }
+
+        void _udpPortScan_RobotResponseEvent(object sender, RobotFinderLibrary.ProgressEventArgs args)
+        {
+            #region 记录这个IP
+            if (string.IsNullOrEmpty(args.ResponseText))
+            {
+                return;
+            }
+
+            string[] tt = args.ResponseText.Replace("Robot_", string.Empty).Split(':');
+            if (tt != null && tt.Length >= 2)
+            {
+                try
+                {
+                    RobotConfigItem rci = new RobotConfigItem();
+                    rci.NickName = args.ResponseText;
+                    rci.IP = tt[0];
+                    rci.Port = int.Parse(tt[1]);
+                    totalRobotList.Add(rci);
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine(ex.ToString());
+                }
+            }
+            #endregion
+
+            //刷新列表
+            UpdateConnectionList();
+        }
+
+        /// <summary>
+        /// 刷新列表
+        /// </summary>
+        private void UpdateConnectionList()
+        {
+            if (IsHandleCreated)
+            {
+                Invoke(new MethodInvoker(delegate()
+                    {
+                        lvConnectionList.Items.Clear();
+
+                        foreach (RobotConfigItem rcii in totalRobotList)
+                        {
+                            ListViewItem lvi = new ListViewItem();
+                            lvi.Text = rcii.NickName;
+                            lvi.SubItems.Add(rcii.IP);
+                            lvi.SubItems.Add(rcii.Port + "");
+
+                            lvi.Tag = rcii;
+
+                            lvConnectionList.Items.Add(lvi);
+                        }
+                    }));
+            }
+        }
+
+        public DeviceListForm DeviceListF { get; set; }
+
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+
+            if (_udpPortScan.UdpClient.LocalUdpPort <= 0)
+            {
+                _udpPortScan.UdpClient.OpenListener();
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            try
+            {
+                _udpPortScan.UdpClient.CloseListener();
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.ToString());
+            }
+        }
 
         private void btnStartScan_Click(object sender, EventArgs e)
         {
-
+            _udpPortScan.ResultDict.Clear();
+            _udpPortScan.InitQueues(tbUdpBoardCastIp.Text.Trim(), 5000, 6000);
+            _udpPortScan.StartScan();
         }
 
         private void btnOK_Click(object sender, EventArgs e)
